da9bfcc5f9a1e747f6b65bb53756d0f4568be6bc
Sean Demura <sean@apcera.com>
2017-01-01 09:52:16 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # TMUX
-ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
 # NVIM
 pip3 install neovim
+pip install neovim
 mkdir -p ~/.config/nvim
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-git clone git@github.com:VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
-git clone git@github.com:sdemura/tmux-sensible.git ~/.dotfiles/tmux/tmux-sensible
+git clone https://github.com:VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim

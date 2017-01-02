8e021e435888285d7426b8b470d1f1ef70baba7f
Sean Demura <sean@apcera.com>
2017-01-01 23:12:09 -0500

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ pip3 install neovim
 pip install neovim
 mkdir -p ~/.config/nvim
 ln -s ~/.dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-git clone https://github.com:VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim

ba40fee4a6c0a900b7e268d08ee15cc97c4bbee0
Sean Demura <sean.demura@corelight.com>
2022-09-28 13:41:33 -0400

    forget wilders

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -360,6 +360,46 @@ return require("packer").startup({
 			end,
 		})
 
+		-- use({
+		-- 	"gelguy/wilder.nvim",
+		-- 	config = function()
+		-- 		-- config goes here
+		-- 		local wilder = require("wilder")
+		-- 		wilder.setup({ modes = { ":", "/", "?" } })
+		--
+		-- 		wilder.set_option("pipeline", {
+		-- 			wilder.branch(
+		-- 				wilder.cmdline_pipeline({
+		-- 					fuzzy = 1,
+		-- 					set_pcre2_pattern = 1,
+		-- 				}),
+		-- 				wilder.python_search_pipeline({
+		-- 					pattern = "fuzzy",
+		-- 				})
+		-- 			),
+		-- 		})
+		--
+		-- 		local highlighters = {
+		-- 			wilder.pcre2_highlighter(),
+		-- 			wilder.basic_highlighter(),
+		-- 		}
+		--
+		-- 		wilder.set_option(
+		-- 			"renderer",
+		-- 			wilder.renderer_mux({
+		-- 				[":"] = wilder.popupmenu_renderer({
+		-- 					highlighter = highlighters,
+		-- 					left = { " ", wilder.popupmenu_devicons() },
+		-- 					right = { " ", wilder.popupmenu_scrollbar() },
+		-- 				}),
+		-- 				["/"] = wilder.wildmenu_renderer({
+		-- 					highlighter = highlighters,
+		-- 				}),
+		-- 			})
+		-- 		)
+		-- 	end,
+		-- })
+
 		if packer_bootstrap then
 			require("packer").sync()
 		end

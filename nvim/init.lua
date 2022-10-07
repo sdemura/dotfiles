567e72fdde7968acd8109845f89143c46658c958
Sean Demura <sean.demura@corelight.com>
2022-10-06 21:18:27 -0400

    default leader

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,7 +34,7 @@ vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 vim.api.nvim_create_autocmd("BufWritePre", { command = "%s/\\s\\+$//e" })
 
 -- keymaps
-vim.g.mapleader = " "
+-- vim.g.mapleader = " "
 
 local opts = { noremap = true, silent = true }
 
@@ -42,20 +42,21 @@ vim.api.nvim_set_keymap("n", "<Esc><Esc>", "<Esc>:nohlsearch<CR><C-l><CR>", opts
 vim.api.nvim_set_keymap("n", "<leader>e", "<cmd>:Neotree toggle<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>E", "<cmd>:Neotree toggle reveal<CR>", opts)
 -- vim.api.nvim_set_keymap("n", "<leader>o", "<cmd>:AerialToggle<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>ff", '<cmd>lua require("telescope.builtin").find_files{hidden=true}<CR>', opts)
-vim.api.nvim_set_keymap("n", "<leader>fw", '<cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
-vim.api.nvim_set_keymap("n", "<leader>fb", '<cmd>lua require("telescope.builtin").buffers()<CR>', opts)
-vim.api.nvim_set_keymap("n", "<leader>fs", '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>', opts)
+vim.api.nvim_set_keymap("n", "<leader>f", '<cmd>lua require("telescope.builtin").find_files{hidden=true}<CR>', opts)
+vim.api.nvim_set_keymap("n", "<leader>g", '<cmd>lua require("telescope.builtin").live_grep()<CR>', opts)
+vim.api.nvim_set_keymap("n", "<leader>b", '<cmd>lua require("telescope.builtin").buffers()<CR>', opts)
+vim.api.nvim_set_keymap("n", "<leader>s", '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>', opts)
 vim.api.nvim_set_keymap("n", "s", ":HopWord<CR>", opts)
 
 vim.api.nvim_set_keymap("n", "<leader>y", '"*y', opts)
-vim.api.nvim_set_keymap("n", "<leader>y", '"*y', opts)
-vim.api.nvim_set_keymap("v", "<leader>p", '"*p', opts)
+vim.api.nvim_set_keymap("n", "<leader>p", '"*p', opts)
+
+vim.api.nvim_set_keymap("v", "<leader>y", '"*y', opts)
 vim.api.nvim_set_keymap("v", "<leader>p", '"*p', opts)
 
 vim.api.nvim_set_keymap("n", "<leader>cc", ":e ~/.config/nvim/init.lua<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>cp", ":e ~/.config/nvim/lua/plugins.lua<CR>", opts)
---
+
 -- place this in one of your configuration file(s)
 vim.api.nvim_set_keymap(
 	"",
@@ -82,7 +83,7 @@ vim.api.nvim_set_keymap(
 	{}
 )
 
-vim.api.nvim_set_keymap("v", "<LeftRelease>", '"*ygv', opts)
+-- vim.api.nvim_set_keymap("v", "<LeftRelease>", '"*ygv', opts)
 
 vim.api.nvim_set_keymap("i", "<C-r>", "<cmd>:Telescope registers<cr>", opts)
 vim.api.nvim_set_keymap("n", '""', "<cmd>:Telescope registers<cr>", opts)
@@ -131,7 +132,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 
-	vim.keymap.set("n", "<space>F", function()
+	vim.keymap.set("n", "<space>f", function()
 		vim.lsp.buf.format({ async = false })
 	end, bufopts)
 end

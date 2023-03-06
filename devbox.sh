36051519e209ac1e709f300b62e1d5e8442ee47f
Sean Demura <sean.demura@corelight.com>
2023-03-06 11:05:32 -0500

    wip

--- devbox.sh
+++ devbox.sh
@@ -31,7 +31,7 @@ sudo apt-get install -y \
 	lsb-release
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo sh gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
 	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \

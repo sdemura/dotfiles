698a99d7ce39a0b862af99d29bb6e9d8ad59054a
Sean Demura <sean.demura@corelight.com>
2025-06-08 06:51:28 -0400

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,8 @@
--- Set mapleader before plugins are loaded
+-- Set global and local -- Set global and local leader keys.
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
---[[ lazy.nvim bootstrap ]]
+-- lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
@@ -10,13 +10,13 @@ if not vim.loop.fs_stat(lazypath) then
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		"--branch=stable",
 		lazypath,
 	})
 end
 vim.opt.rtp:prepend(lazypath)
 
---[[ Plugin Specifications ]]
+-- Plugin Specifications
 require("lazy").setup({
 	{
 		"hedyhli/outline.nvim",
@@ -30,23 +30,22 @@ require("lazy").setup({
 	{ "tpope/vim-eunuch" },
 	{ "yorickpeterse/nvim-tree-pairs", opts = {} },
 
-	-- LSP, Completion, and Formatter Plugins (lsp-zero removed)
-	{ "neovim/nvim-lspconfig" }, -- Core LSP configuration
-	{ "hrsh7th/nvim-cmp" }, -- Autocompletion engine
-	{ "hrsh7th/cmp-nvim-lsp" }, -- LSP source for nvim-cmp
-	{ "hrsh7th/cmp-buffer" }, -- Buffer source for nvim-cmp
-	{ "hrsh7th/cmp-path" }, -- Path source for nvim-cmp
-	{ "L3MON4D3/LuaSnip" }, -- Snippet engine
-	{ "saadparwaiz1/cmp_luasnip" }, -- Snippet source for nvim-cmp
-	{ "onsails/lspkind.nvim" }, -- Adds icons to completion menu
+	-- LSP, Completion, and Formatter Plugins
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "L3MON4D3/LuaSnip" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "onsails/lspkind.nvim" },
 
-	-- Replaces null-ls with a modern, maintained formatter plugin
 	{
 		"stevearc/conform.nvim",
 		event = { "BufWritePre" },
+		module = "conform",
 		cmd = { "ConformInfo" },
 		opts = {
-			-- Define your formatters here
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "isort", "black" },
@@ -59,10 +58,7 @@ require("lazy").setup({
 				json = { "prettier" },
 				yaml = { "prettier" },
 				markdown = { "prettier" },
-				-- For hadolint (linter), you would use nvim-lint,
-				-- but for formatting, conform.nvim is the tool.
 			},
-			-- Automatically format on save
 			format_on_save = {
 				timeout_ms = 500,
 				lsp_fallback = true,
@@ -71,14 +67,17 @@ require("lazy").setup({
 	},
 
 	{ "folke/neodev.nvim", opts = {} },
-	{ "j-hui/fidget.nvim", opts = {} },
 	{
-		"ray-x/go.nvim",
-		dependencies = { "ray-x/guihua.lua", "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter" },
-		opts = { lsp_inlay_hints = { enable = false } },
-		ft = { "go", "gomod" },
-		build = ':lua require("go.install").update_all_sync()',
+		"j-hui/fidget.nvim",
+		opts = { notification = { override_vim_notify = true, window = { winblend = 0 } } },
 	},
+	-- {
+	-- 	"ray-x/go.nvim",
+	-- 	dependencies = { "ray-x/guihua.lua", "neovim/nvim-lspconfig", "nvim-treesitter/nvim-treesitter" },
+	-- 	opts = { lsp_inlay_hints = { enable = false } },
+	-- 	ft = { "go", "gomod" },
+	-- 	build = ':lua require("go.install").update_all_sync()',
+	-- },
 
 	{ "lewis6991/gitsigns.nvim", opts = {} },
 	{ "vladdoster/remember.nvim", opts = {} },
@@ -232,7 +231,7 @@ require("lazy").setup({
 	},
 }, {})
 
---- options
+--- Options ---
 vim.opt.breakindent = true
 vim.opt.cmdheight = 1
 vim.opt.completeopt = "menu,menuone,noselect"
@@ -260,7 +259,7 @@ vim.opt.updatetime = 250
 vim.opt.wildignore = vim.opt.wildignore + { "*/.git/*", "*/.hg/*", "*/.DS_Store", "*.o", "*.pyc" }
 vim.opt.wrap = false
 
--- disable some builtin plugins we don't use
+-- Disable specific built-in plugins
 vim.g.editorconfig = false
 vim.g.loaded_man = false
 vim.g.loaded_matchit = 1
@@ -280,8 +279,7 @@ vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 
---[[ LSP & COMPLETION SETUP (Manual) ]]
-
+-- LSP & COMPLETION SETUP
 local cmp = require("cmp")
 local lspconfig = require("lspconfig")
 local lspkind = require("lspkind")
@@ -315,32 +313,37 @@ cmp.setup({
 	},
 })
 
--- Add borders to floating windows
+-- Add rounded borders to LSP floating windows.
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
--- This function will be called for each server that is setup
-local on_attach = function(client, bufnr)
+-- Callback function executed when an LSP client attaches to a buffer.
+local on_attach = function(_, bufnr)
 	local opts = { buffer = bufnr, noremap = true, silent = true }
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-	vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-	vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover information", buffer = bufnr }) -- Show hover information for the word under the cursor
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition", buffer = bufnr }) -- Go to definition
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration", buffer = bufnr }) -- Go to declaration
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation", buffer = bufnr }) -- Go to implementation
+	vim.keymap.set("n", "go", vim.lsp.buf.type_definition, { desc = "Go to type definition", buffer = bufnr }) -- Go to type definition
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "List references", buffer = bufnr }) -- List references
+	vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, { desc = "Show signature help", buffer = bufnr }) -- Show signature help
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { desc = "Rename symbol", buffer = bufnr }) -- Rename symbol
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Trigger code action", buffer = bufnr }) -- Trigger code action
+	vim.keymap.set(
+		"n",
+		"<leader>e",
+		vim.diagnostic.open_float,
+		{ desc = "Open floating diagnostic window", buffer = bufnr }
+	) -- Open floating diagnostic window
 
-	-- toggle for virtual lines
 	vim.keymap.set("n", "gK", function()
-		local current_config = vim.diagnostic.config().virtual_text
-		vim.diagnostic.config({ virtual_text = not current_config })
+		local current_config = vim.diagnostic.config().virtual_lines
+		vim.diagnostic.config({ virtual_lines = not current_config })
 	end, { desc = "Toggle diagnostic virtual text" })
 
 	vim.diagnostic.config({
 		virtual_text = false,
+		virtual_lines = false,
 		update_in_insert = false,
 		underline = true,
 		severity_sort = true,
@@ -355,10 +358,9 @@ local on_attach = function(client, bufnr)
 	})
 end
 
--- Set the capabilities for nvim-cmp
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
--- Define server-specific settings
+-- Define server-specific LSP configurations.
 local servers = {
 	gopls = {
 		settings = {
@@ -405,7 +407,6 @@ local servers = {
 	pyright = {},
 }
 
--- Loop through servers and set them up
 for server_name, custom_opts in pairs(servers) do
 	lspconfig[server_name].setup(vim.tbl_deep_extend("force", {
 		on_attach = on_attach,
@@ -413,37 +414,33 @@ for server_name, custom_opts in pairs(servers) do
 	}, custom_opts or {}))
 end
 
---[[ Custom Commands & Autocommands ]]
-
--- gbrowse to work with gitlab
+-- Custom Commands & Autocommands
 vim.api.nvim_create_user_command("Browse", function(gopts)
 	vim.fn.system({ "open", gopts.fargs[1] })
-end, { nargs = 1 })
+end, { nargs = 1, desc = "Open given URL in default browser" })
 
--- trail whitespace on save
-vim.api.nvim_create_autocmd("BufWritePre", { command = ":lua MiniTrailspace.trim()" })
+vim.api.nvim_create_autocmd(
+	"BufWritePre",
+	{ command = ":lua MiniTrailspace.trim()", desc = "Trim trailing whitespace" }
+)
 
--- NOTE: The goimports on save functionality is now handled by conform.nvim's `format_on_save` option above.
--- The autocommand group below is no longer necessary.
--- local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
--- vim.api.nvim_create_autocmd("BufWritePre", {
--- 	pattern = "*.go",
--- 	callback = function()
--- 		require("go.format").goimport()
--- 	end,
--- 	group = format_sync_grp,
--- })
-
--- quickly move back to git root
 vim.api.nvim_create_user_command("CdGitRoot", function()
-	local git_root = vim.fn.system("git rev-parse --show-toplevel")
-	git_root = git_root:match("^%s*(.-)%s*$")
-	vim.api.nvim_set_current_dir(git_root)
-	vim.notify("changed dir to " .. git_root)
-end, {})
+	local handle = io.popen("git rev-parse --show-toplevel 2> /dev/null")
+	local git_root = ""
+	if handle then -- Check if handle is not nil
+		git_root = handle:read("*a"):match("^%s*(.-)%s*$")
+		handle:close()
+	end
 
---[[ Final Setup (Theme, UI, Keymaps) ]]
+	if git_root and git_root ~= "" then
+		vim.api.nvim_set_current_dir(git_root)
+		vim.notify("Changed directory to " .. git_root, vim.log.levels.INFO)
+	else
+		vim.notify("Not in a Git repository.", vim.log.levels.WARN)
+	end
+end, { desc = "Change current directory to Git repository root" })
 
+-- Final Setup (Theme, UI, Keymaps)
 vim.cmd.colorscheme("catppuccin-frappe")
 
 require("bufferline").setup({
@@ -456,61 +453,68 @@ require("bufferline").setup({
 })
 
 local key_opts = { noremap = true, silent = true }
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-vim.keymap.set("n", "<Esc><Esc>", "<cmd>:nohlsearch<CR><C-l><CR>", key_opts)
-vim.keymap.set("n", "<leader>lu", "<cmd>:Lazy update<CR>", key_opts)
-vim.keymap.set("n", "q", "<Nop>", key_opts) -- disable macros
-vim.keymap.set({ "n", "v", "i" }, "<F1>", "<Nop>", key_opts) -- disable f1 help]
-vim.keymap.set("n", "<leader>r", "<cmd>:CdGitRoot<CR>", key_opts)
+
+-- Keymaps:
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true, desc = "Leader key" })
+vim.keymap.set("n", "<Esc><Esc>", "<cmd>:nohlsearch<CR><C-l><CR>", key_opts) -- Clear search highlighting and redraw screen
+vim.keymap.set("n", "<leader>lu", "<cmd>:Lazy update<CR>", { desc = "Update Lazy plugins" })
+vim.keymap.set("n", "q", "<Nop>", key_opts) -- Disable the 'q' key
+vim.keymap.set({ "n", "v", "i" }, "<F1>", "<Nop>", key_opts) -- Disable the <F1> key (help)
+vim.keymap.set("n", "<leader>r", "<cmd>:CdGitRoot<CR>", { desc = "Change directory to Git root" })
 
 vim.keymap.set("n", "<leader>yf", function()
 	local path = vim.fn.expand("%:p")
 	vim.fn.setreg("+", path)
 	vim.notify("Copied path to clipboard:\n" .. path, vim.log.levels.INFO, { title = "Yank File Path" })
-end, { noremap = true, silent = true, desc = "Yank full file path" })
+end, { noremap = true, silent = true, desc = "Yank full file path to clipboard" })
 
--- Keymap for conform.nvim (replaces null-ls formatting keymap)
-local function format_with_notify()
+vim.keymap.set({ "n", "v" }, "<leader>F", function()
 	require("conform").format({
 		async = true,
 		lsp_fallback = true,
 		timeout_ms = 1000,
-		-- Called after formatting completes
-		callback = function(ok)
-			if ok then
-				vim.notify("Formatting completed", vim.log.levels.INFO, { title = "Conform" })
-			else
-				vim.notify("Formatting failed", vim.log.levels.ERROR, { title = "Conform" })
-			end
-		end,
 	})
-end
+end, { desc = "Format buffer (manual)" })
 
-vim.keymap.set({ "n", "v" }, "<leader>F", format_with_notify, { desc = "Format buffer" })
-vim.keymap.set({ "n", "v" }, "<F3>", format_with_notify, { desc = "Format buffer" })
+vim.keymap.set({ "n", "v" }, "<F3>", function()
+	require("conform").format({
+		async = true,
+		lsp_fallback = true,
+		timeout_ms = 1000,
+	})
+end, { desc = "Format buffer (manual)" })
 
--- fzf keybinds
-vim.keymap.set("n", "<leader><leader>", "<cmd>:FzfLua<cr>", key_opts)
-vim.keymap.set("n", "<leader>f", "<cmd>:FzfLua files<CR>", key_opts)
-vim.keymap.set("n", "<leader>g", "<cmd>:FzfLua live_grep_glob<CR>", key_opts)
-vim.keymap.set("n", "<leader>G", "<cmd>:FzfLua grep_curbuf<CR>", key_opts)
-vim.keymap.set("n", "<leader>s", "<cmd>:FzfLua lsp_document_symbols<CR>", key_opts)
-vim.keymap.set("n", "<leader>d", "<cmd>:FzfLua lsp_workspace_diagnostics<CR>", key_opts)
-vim.keymap.set("n", "<leader>cc", "<cmd>:FzfLua files cwd=~/.config<CR>", key_opts)
-vim.keymap.set("n", "<leader>b", "<cmd>:FzfLua buffers<cr>", key_opts)
-vim.keymap.set("n", '<leader>"', "<cmd>:FzfLua registers<cr>", key_opts)
-vim.keymap.set("n", "<leader>'", "<cmd>:FzfLua marks<cr>", key_opts)
-vim.keymap.set("i", "<C-r>", "<cmd>:FzfLua registers<cr>", key_opts)
+-- fzf-lua keybinds:
+vim.keymap.set("n", "<leader><leader>", "<cmd>:FzfLua<cr>", { desc = "Open FzfLua default fuzzy finder" })
+vim.keymap.set("n", "<leader>f", "<cmd>:FzfLua files<CR>", { desc = "Find files using FzfLua" })
+vim.keymap.set("n", "<leader>g", "<cmd>:FzfLua live_grep_glob<CR>", { desc = "Live grep across files" })
+vim.keymap.set("n", "<leader>G", "<cmd>:FzfLua grep_curbuf<CR>", { desc = "Grep in current buffer" })
+vim.keymap.set("n", "<leader>s", "<cmd>:FzfLua lsp_document_symbols<CR>", { desc = "List LSP document symbols" })
+vim.keymap.set(
+	"n",
+	"<leader>d",
+	"<cmd>:FzfLua lsp_workspace_diagnostics<CR>",
+	{ desc = "List LSP workspace diagnostics" }
+)
+vim.keymap.set("n", "<leader>cc", "<cmd>:FzfLua files cwd=~/.config<CR>", { desc = "Find files in ~/.config" })
+vim.keymap.set("n", "<leader>b", "<cmd>:FzfLua buffers<cr>", { desc = "List open buffers" })
+vim.keymap.set("n", '<leader>"', "<cmd>:FzfLua registers<cr>", { desc = "Show registers" })
+vim.keymap.set("n", "<leader>'", "<cmd>:FzfLua marks<cr>", { desc = "Show marks" })
+vim.keymap.set("i", "<C-r>", "<cmd>:FzfLua registers<cr>", { desc = "Show registers (insert mode)" })
 
--- outline
-vim.keymap.set("n", "<leader>o", "<cmd>:Outline<CR>", key_opts)
--- hop
-vim.keymap.set("n", "s", "<cmd>:HopWord<CR>", key_opts)
--- neotree
-vim.keymap.set("n", "-", "<cmd>:Neotree toggle<CR>", key_opts)
-vim.keymap.set("n", "_", "<cmd>:Neotree toggle reveal<CR>", key_opts)
--- quickfix stuff
-vim.keymap.set("n", "<leader>cn", "<cmd>:cnext<CR>")
-vim.keymap.set("n", "<leader>cp", "<cmd>:cprev<CR>")
--- git browse
-vim.keymap.set("n", "<leader>B", "<cmd>:GBrowse<CR>")
+-- outline.nvim keybinds:
+vim.keymap.set("n", "<leader>o", "<cmd>:Outline<CR>", { desc = "Open Outline window" })
+
+-- hop.nvim keybinds:
+vim.keymap.set("n", "s", "<cmd>:HopWord<CR>", { desc = "Jump to a word using Hop" })
+
+-- neo-tree.nvim keybinds:
+vim.keymap.set("n", "-", "<cmd>:Neotree toggle<CR>", { desc = "Toggle Neo-tree file explorer" })
+vim.keymap.set("n", "_", "<cmd>:Neotree toggle reveal<CR>", { desc = "Toggle Neo-tree and reveal current file" })
+
+-- Quickfix list keybinds:
+vim.keymap.set("n", "<leader>cn", "<cmd>:cnext<CR>", { desc = "Next quickfix item" })
+vim.keymap.set("n", "<leader>cp", "<cmd>:cprev<CR>", { desc = "Previous quickfix item" })
+
+-- Git browse keybind:
+vim.keymap.set("n", "<leader>B", "<cmd>:GBrowse<CR>", { desc = "Open current file in browser" })

492d18beb946be8ba3483825e4c21d45bb2c66c2
sean.sd.demura <sean.demura@corelight.com>
2026-03-30 09:54:22 -0400

    Replace lazy.nvim bootstrap with vim.pack

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,25 +3,11 @@ require("options")
 require("keymaps")
 require("autocmds")
 
--- lazy.nvim bootstrap
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
+-- Plugin hooks (must be defined before vim.pack.add)
+require("pack-hooks")
 
--- Setup lazy.nvim with plugins from an external file
-require("lazy").setup(require("plugins"), {})
+-- Install and load plugins
+require("plugins")
 
 -- Setup configurations
 require("completion").setup()
